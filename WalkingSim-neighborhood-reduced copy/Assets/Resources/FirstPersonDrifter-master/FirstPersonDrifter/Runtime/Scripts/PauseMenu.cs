6a2ab067c292530daa6449753a404d03b3226149
George Still <georgest32@gmail.com>
2020-10-28 00:21:26 -0400

    added happy house to project, linked together with forest scene

---
+++ WalkingSim-neighborhood-reduced copy/Assets/Resources/FirstPersonDrifter-master/FirstPersonDrifter/Runtime/Scripts/PauseMenu.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+    
+    private static bool _gameIsPaused;
+
+    private void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _gameIsPaused = false;
+        LockCursor(true);
+    }
+
+    private void Pause()
+    {
+        if (!_gameIsPaused)
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            _gameIsPaused = true;
+            LockCursor(false);
+        } else Resume();
+    }
+
+    public void OnPause(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) Pause();
+    }
+
+    public void OnResume()
+    {
+        Resume();
+    }
+
+    public void OnOptionsMenu()
+    {
+        optionsMenuUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void OnReset()
+    {
+        Resume();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    private void LockCursor(bool lockCursor)
+    {
+        Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !lockCursor;
+    }
+}
